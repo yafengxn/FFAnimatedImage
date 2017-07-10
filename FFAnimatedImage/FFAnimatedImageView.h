3762e3888367bfdf765adba16f8efc44a0d259f0
fengxiaoning <fengxiaoning@yongche.com>
2017-07-10 13:52:47 +0800

    add new file

---
+++ FFAnimatedImage/FFAnimatedImageView.h
@@ -0,0 +1,13 @@
+//
+//  FFAnimatedImageView.h
+//  FFAnimatedImageDemo
+//
+//  Created by yafengxn on 2017/7/10.
+//  Copyright © 2017年 yafengxn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FFAnimatedImageView : UIImageView
+
+@end
---
+++ FFAnimatedImage/FFAnimatedImageView.m
@@ -0,0 +1,21 @@
+//
+//  FFAnimatedImageView.m
+//  FFAnimatedImageDemo
+//
+//  Created by yafengxn on 2017/7/10.
+//  Copyright © 2017年 yafengxn. All rights reserved.
+//
+
+#import "FFAnimatedImageView.h"
+
+@implementation FFAnimatedImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
